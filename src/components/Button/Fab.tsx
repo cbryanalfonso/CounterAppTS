36eca5745cec6e5b0a4aab78b862362355ad40a5
cbryanalfonso <cbryanalfonso@gmail.com>
2022-04-20 11:57:28 -0500

    Finalizacion counter APP ios y android

--- src/components/Button/Fab.tsx
+++ src/components/Button/Fab.tsx
@@ -1,22 +1,97 @@
 import React from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  View,
+  Platform,
+} from 'react-native';
 
-const Fab = () => {
-  return (
-    <>
-      <TouchableOpacity onPress={() => {}}>
-        <View style={styles.btn}>
-          <Text>+1</Text>
-        </View>
+interface Props {
+  title: string;
+  position?: 'br' | 'bl';
+  onPress: () => void;
+}
+
+const Fab = ({title, onPress, position = 'br'}: Props) => {
+  const ios = () => {
+    return(
+      <TouchableOpacity 
+      activeOpacity={ 0.75 }
+      onPress={onPress}
+      style={[
+        styles.fabLocation,
+        position === 'bl' ? styles.left : styles.right,
+      ]}>
+       
+          <View style={styles.fab}>
+            <Text style={styles.fabTxt}>{title}</Text>
+          </View>
+  
       </TouchableOpacity>
-    </>
-  );
+    )
+
+  };
+
+  const android = () => {
+    return (
+      <View
+        style={[
+          styles.fabLocation,
+          position === 'bl' ? styles.left : styles.right,
+        ]}>
+        <TouchableNativeFeedback
+          background={TouchableNativeFeedback.Ripple('#28425B', false, 30)}
+          onPress={onPress}>
+          <View style={styles.fab}>
+            <Text style={styles.fabTxt}>{title}</Text>
+          </View>
+        </TouchableNativeFeedback>
+      </View>
+    );
+  };
+
+  return (Platform.OS ==='ios') ? ios() : android()
 };
 
 const styles = StyleSheet.create({
   btn: {
     backgroundColor: 'red',
     borderRadius: 100,
+  },
+  fab: {
+    backgroundColor: 'red',
+    borderRadius: 100,
+    width: 60,
+    height: 60,
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.37,
+    shadowRadius: 7.49,
+
+    elevation: 12,
+  },
+  fabTxt: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'white',
+    alignSelf: 'center',
+  },
+  fabLocation: {
+    position: 'absolute',
+    bottom: 30,
+    //right: 30,
+  },
+  right: {
+    right: 25,
+  },
+  left: {
+    left: 25,
   },
 });
 
--- src/screens/ContadorScreen.tsx
+++ src/screens/ContadorScreen.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {Button, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import Fab from '../components/Button/Fab';
 
 const ContadorScreen = () => {
   const [contador, setContador] = useState(10);
@@ -8,17 +9,17 @@ const ContadorScreen = () => {
     <View style={styles.container}>
       <Text style={styles.title}>Contador : {contador}</Text>
 
-      <TouchableOpacity
-        style={styles.fabLocationBL}
-        onPress={() => {
-          setContador(contador - 1);
-        }}>
-        <View style={styles.fab}>
-          <Text style={styles.fabTxt}>-1</Text>
-        </View>
-      </TouchableOpacity>
+      <Fab
+        title = '-1'
+        onPress={()=> setContador(contador - 1)}
+        position= "bl"
+      />
+       <Fab
+        title = '+1'
+        onPress={()=> setContador(contador + 1)}
+      />
 
-      <TouchableOpacity
+     {/*  <TouchableOpacity
         style={styles.fabLocationBR}
         onPress={() => {
           setContador(contador + 1);
@@ -26,7 +27,7 @@ const ContadorScreen = () => {
         <View style={styles.fab}>
           <Text style={styles.fabTxt}>+1</Text>
         </View>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
 
     </View>
   );
@@ -42,29 +43,7 @@ const styles = StyleSheet.create({
     fontSize: 40,
     top: -10,
   },
-  fab: {
-    backgroundColor: 'red',
-    borderRadius: 100,
-    width: 60,
-    height: 60,
-    justifyContent: 'center',
-  },
-  fabTxt: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: 'white',
-    alignSelf: 'center',
-  },
-  fabLocationBR: {
-    position: 'absolute',
-    bottom: 30,
-    right: 30,
-  },
-  fabLocationBL:{
-    position: 'absolute',
-    bottom: 30,
-    left: 30,
-  }
+  
 });
 
 export default ContadorScreen;
